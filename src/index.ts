758301184182f5e816293654905d851c85a71b5d
Your Name <your@email.com>
2026-03-05 23:54:39 +0530

    docs: add project README

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ const app = express()
 app.use(express.json())
 
 app.use("/identify", identifyRoute)
-
+app.get("/", (req, res) => {
+  res.json({
+    msg: "BiteSpeed Identity Reconciliation API running"
+  }).status(200)
+})
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
